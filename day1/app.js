2441ae1867eedf07c37ee0d32f91f2035facad03
jamesjoel <james.joelj@gmail.com>
2018-07-13 12:08:09 +0530

    first commig

---
+++ day1/app.js
@@ -0,0 +1,5 @@
+var data = require("./data");
+// var ans = x.test(20);
+// console.log(ans.sum);
+var obj=data();
+obj.test(obj.a);
---
+++ day1/data.js
@@ -0,0 +1,32 @@
+// var obj = { 
+// 			a : "rohit", 
+// 			b : "james",
+// 			test : function(a){
+// 				return { sum : a+a, mul : a*a };
+// 			}
+// 		};
+
+// module.exports=obj;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports=function(){
+	return {
+		a : "rohit",
+		b : "james",
+		test : function(a){
+			console.log("hello....", a);
+		}
+	}
+}
