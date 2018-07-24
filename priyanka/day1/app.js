06bd92e248c2b14e72898e1cbd101ad2809d902c
jamesjoel <james.joelj@gmail.com>
2018-07-24 23:50:34 +0530

    layout system

--- priyanka/day1/app.js
+++ priyanka/day1/app.js
@@ -6,15 +6,27 @@ var express = require('express');
 var app = express();
 
 
+app.set("view engine", "ejs");
+// app.set('views', "views");
+
+
 app.get("/", function(req, res){
-	// console.log(__dirname);
-	res.sendFile(__dirname+"/home.html");
+	var pagedata = { title : "Home Page", pagename : "home"};
+	res.render("layout", pagedata);
 });
 
+
 app.get("/about", function(req, res){
-	// console.log(__dirname);
-	res.sendFile(__dirname+"/about.html");
+	var pagedata = { title : "About Page", pagename : "about"};
+	res.render("layout", pagedata);
 });
+
+app.get("/contact", function(req, res){
+	var pagedata = { title : "Contact Page", pagename : "contact"};
+	res.render("layout", pagedata);
+});
+
+
 
 
 
